1d6c042552587efed5865d30ea25fe0920e5e096
Apoorva Kumar <apkvandagadde2@gmail.com>
2022-05-03 15:36:44 +0530

    shit fixed

--- src/views/WhitePaper.vue
+++ src/views/WhitePaper.vue
@@ -3,8 +3,8 @@
            <img alt="Marley Inu" src="../assets/dollar.png" class="lg:w-1/4 w-fit animate__animated animate__fadeInDown animate__fast">
     </div>
     <div  class="flex items-center justify-center m-1" >
-        <h1 class="inline-flex text-6xl sm:text-5xl text-amber-400  font-extrabold font-rubik animate__animated animate__fadeInLeft animate__fast uppercase ">Marley&nbsp; </h1>
-        <h1 class="inline-flex text-6xl sm:text-5xl text-amber-400  font-bold font-rubik animate__animated animate__fadeInRight animate__fast uppercase ">Inu</h1>
+        <h1 class="inline-flex text-6xl sm:text-5xl text-amber-400  font-extrabold font-rubik animate__animated animate__fadeInLeft animate__fast uppercase ">Comming&nbsp; </h1>
+        <h1 class="inline-flex text-6xl sm:text-5xl text-amber-400  font-bold font-rubik animate__animated animate__fadeInRight animate__fast uppercase ">Soon</h1>
     </div>
 </template>
 
