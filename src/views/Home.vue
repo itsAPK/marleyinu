fee24896370387aac1eb762fe50848488b21d084
Apoorva Kumar <apkvandagadde2@gmail.com>
2022-05-05 11:52:47 +0530

    chart and buy button updated

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,14 +13,15 @@
     </h2>
    <div class=" items-center justify-between px-10 py-5 mt-5 lg:px-32 lg:py-10 columns-xl">
      <p class="text-justify text-amber-400 lg:text-2xl font-lexend  uppercase mt-5 lg:mt-16 ">Introduction</p>
-     <div class="">
     <div class="inline-flex">
-     <p class=" text-neutral-100 lg:text-justify text-sm  font-raleway font-black uppercase mt-4 ">
+     <p class=" text-neutral-100 lg:text-justify text-sm  font-raleway font-black uppercase mt-4  ">
     Marley Inu $minu is built by a strong team of crypto experts. We aim to build a sustainable, long term memetoken bringing innovative ideas in the  cryptospace. In addition, we aim to build a complete ecosystem with our utilities and this can be accomplished by a strong and connected community.
     </p>
     </div>
+     <div class="lg:flex items-center justify-between py-10 m-2">
     <div class="inline-flex">
-     <img alt="Marley Inu" src="../assets/dollar.png" width="250" class=" animate__animated animate__fadeInRight animate__fast mt-2 lg:mt-0 ">
+          <button class="h-10 px-6 mr-8 text-yellow-500 text-xl font-lexend400 transition-colors duration-150 border border-yellow-500 rounded-sm focus:shadow-outline hover:bg-yellow-500 hover:text-black"><a href="https://www.dextools.io/app/bsc/pair-explorer/0xAD3d2c978eA1a24a3D895001e4E8db3e04A07401">CHART</a></button>
+            <button class="h-10 px-8 text-yellow-500  text-xl font-lexend400 transition-colors duration-150 border border-yellow-500 rounded-sm focus:shadow-outline hover:bg-yellow-500 hover:text-black"><a href="https://pancakeswap.finance/swap?outputCurrency=0x1FAFBFb8d51c8Dbb66B5b59DaeCcE14b82F5b0d9">BUY</a></button>
     </div>
   </div></div>
   </section>
