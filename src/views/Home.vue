fa993b22538adc5990e2f4ecf193f76b2e7b009f
Apoorva Kumar <apkvandagadde2@gmail.com>
2022-05-05 17:36:22 +0530

    chart and buy button updated

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -18,7 +18,12 @@
     Marley Inu $minu is built by a strong team of crypto experts. We aim to build a sustainable, long term memetoken bringing innovative ideas in the  cryptospace. In addition, we aim to build a complete ecosystem with our utilities and this can be accomplished by a strong and connected community.
     </p>
     </div>
-     <div class="lg:flex items-center justify-between pt-10">
+    <div class="lg:flex items-center justify-between pt-10">
+<button type="button" class="text-black bg-gradient-to-r from-yellow-400 via-yellow-500 to-yellow-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-yellow-300 dark:focus:ring-yellow-800 shadow-lg shadow-yellow-500/50 dark:shadow-lg dark:shadow-yellow-800/80  rounded-2xl text-sm lg:text-xs px-5 py-2.5 text-center font-extrabold mr-2 mb-2">
+            <a href="https://bscscan.com/token/0x1fafbfb8d51c8dbb66b5b59daecce14b82f5b0d9">0x1fafbfb8d51c8dbb66b5b59daecce14b82f5b0d9</a>
+          </button>
+    </div>
+     <div class="lg:flex items-center justify-between pt-10 lg:ml-7">
     <div class="inline-flex">
           <button class="h-10 px-6 mr-8 text-yellow-500 text-xl font-lexend400 transition-colors duration-150 border border-yellow-500 rounded-sm focus:shadow-outline hover:bg-yellow-500 hover:text-black">
             <a href="https://poocoin.app/tokens/0x1fafbfb8d51c8dbb66b5b59daecce14b82f5b0d9">CHART</a>
