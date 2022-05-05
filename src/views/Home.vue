7b9b4a3c7dd080ab985968c9b55e7a0fcc72d594
Apoorva Kumar <apkvandagadde2@gmail.com>
2022-05-05 13:04:17 +0530

    chart and buy button updated

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
 <NavBar/>
   <section id='home'>
-    <div  class="flex items-center justify-center m-5" >
+    <div  class="flex items-center justify-center m-2" >
            <img alt="Marley Inu" src="../assets/logo.png" class="lg:w-1/4 w-fit animate__animated animate__fadeInDown animate__fast">
     </div>
     <div  class="flex items-center justify-center m-1" >
@@ -11,21 +11,27 @@
     <h2 class="text-yellow-600 font-black animate__animated  animate__fadeInUp animate__fast uppercase font-electrolize ">
       A decentralized deflationary memetoken
     </h2>
-   <div class=" items-center justify-between px-10 py-5 mt-5 lg:px-32 lg:py-10 columns-xl">
+   <div class=" items-center justify-between px-10 py-5 mt-5 lg:px-32 lg:py-10">
      <p class="text-justify text-amber-400 lg:text-2xl font-lexend  uppercase mt-5 lg:mt-16 ">Introduction</p>
     <div class="inline-flex">
      <p class=" text-neutral-100 lg:text-justify text-sm  font-raleway font-black uppercase mt-4  ">
     Marley Inu $minu is built by a strong team of crypto experts. We aim to build a sustainable, long term memetoken bringing innovative ideas in the  cryptospace. In addition, we aim to build a complete ecosystem with our utilities and this can be accomplished by a strong and connected community.
     </p>
     </div>
-     <div class="lg:flex items-center justify-between py-10 m-2">
+     <div class="lg:flex items-center justify-between pt-10">
     <div class="inline-flex">
           <button class="h-10 px-6 mr-8 text-yellow-500 text-xl font-lexend400 transition-colors duration-150 border border-yellow-500 rounded-sm focus:shadow-outline hover:bg-yellow-500 hover:text-black">
             <a href="https://poocoin.app/tokens/0x1fafbfb8d51c8dbb66b5b59daecce14b82f5b0d9">CHART</a>
           </button>
-            <button class="h-10 px-8 text-yellow-500  text-xl font-lexend400 transition-colors duration-150 border border-yellow-500 rounded-sm focus:shadow-outline hover:bg-yellow-500 hover:text-black"><a href="https://pancakeswap.finance/swap?outputCurrency=0x1FAFBFb8d51c8Dbb66B5b59DaeCcE14b82F5b0d9">BUY</a></button>
+          <button class="h-10 px-8 text-yellow-500  text-xl font-lexend400 transition-colors duration-150 border border-yellow-500 rounded-sm focus:shadow-outline hover:bg-yellow-500 hover:text-black">
+            <a href="https://pancakeswap.finance/swap?outputCurrency=0x1FAFBFb8d51c8Dbb66B5b59DaeCcE14b82F5b0d9">BUY</a>
+          </button>
     </div>
   </div></div>
+
+   <div  class="flex items-center justify-center" >
+           <img alt="Marley Inu" src="../assets/dollar.png" class="lg:w-1/5 w-4/6 animate__animated animate__fadeInDown animate__fast">
+    </div>
   </section>
   <Tokenomics/>
   <Roadmap/>
